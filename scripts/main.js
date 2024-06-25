996231b3b449db08d0a7f3be616548dd3001512a
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-25 21:43:28 +0200

    feat(js): script code completed page now works

--- scripts/main.js
+++ scripts/main.js
@@ -26,5 +26,35 @@ createApp({
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 } 
             ] ,
+
+            gameDisplayedIndex: 0,
+
+            timerID: undefined,
+            autoScrollIsStopped: false,
+        }
     },
+
+    methods: {
+        activeTheAdjacent(isOrderFromFirstToLast = true) {
+            if (isOrderFromFirstToLast) {
+                this.gameDisplayedIndex = this.gameDisplayedIndex + 1 < this.games.length ? ++this.gameDisplayedIndex : 0;
+            } else {
+                this.gameDisplayedIndex = this.gameDisplayedIndex - 1 >= 0 ? --this.gameDisplayedIndex : this.games.length - 1;
+            }
+        },
+
+        autoScroll(isPlay = true) {
+            if(!this.autoScrollIsStopped) {
+                clearInterval(this.timerID);
+                
+                if(isPlay) {
+                    this.timerID = setInterval(() => this.activeTheAdjacent() , "3000");
+                }
+            }
+        }
+    },
+
+    mounted() {
+        this.autoScroll();
+    }
 }).mount('#app');
